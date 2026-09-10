877593b347d2414e0a1ed25aefeda7ecbcb33122
ravi <ravirajutavaragi@gmail.com>
2026-09-10 23:48:15 +0530

    Added LoopPractice Java programs

---
+++ Wed09SEP/Demo.java
@@ -0,0 +1,18 @@
+package Wed09SEP;
+
+public class Demo
+{
+    static void display()
+    {
+        System.out.println("Hello");
+    } 
+
+
+
+    public static void main(String[] args) 
+    {
+        display();
+        
+    }
+    
+}

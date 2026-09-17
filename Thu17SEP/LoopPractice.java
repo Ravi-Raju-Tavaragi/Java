a79e368875366b0e6b17ffbd9691f281c177987e
ravi <ravirajutavaragi@gmail.com>
2026-09-17 21:52:16 +0530

    Aded Festival home work Practice Java programs

---
+++ Thu17SEP/LoopPractice.java
@@ -0,0 +1,19 @@
+package Thu17SEP;
+
+public class LoopPractice 
+{
+    public static void main(String args)
+    {
+        for (int i = 1; i <= 10; i++) 
+        {
+
+            if (i % 2 == 0) 
+            {
+                continue;
+            }
+
+            System.out.print(i + " ");
+        }
+    }
+    
+}

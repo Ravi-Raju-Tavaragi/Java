aec655419f280ce2003432612c5a9d8cea4d379c
ravi <ravirajutavaragi@gmail.com>
2026-09-18 11:30:17 +0530

    Java Function programs

---
+++ Fri18SEP/Functionclass.java
@@ -0,0 +1,34 @@
+package Fri18SEP;
+
+
+public class Functionclass
+{
+    public static void makeItHalfNumber(int number)
+    {
+        number = number / 2;
+
+    }
+
+    public static int makeItHalfNumber2(int number2)
+    {
+        number2 = number2 / 2;
+        return number2;
+
+    }
+    public static void main(String[] args) 
+    {
+       int number = 10;
+
+       makeItHalfNumber(number);
+
+       System.out.println(number);
+
+       int number2 = 20;
+       number2 = makeItHalfNumber2(number2);
+       System.out.println(number2);
+
+
+        
+    }
+    
+}

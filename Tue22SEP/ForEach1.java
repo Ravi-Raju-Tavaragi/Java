36b0c10e5edbe9013f25b0e4975ee921ca7cab1e
ravi <ravirajutavaragi@gmail.com>
2026-09-22 11:22:41 +0530

    Java Function programs

---
+++ Tue22SEP/ForEach1.java
@@ -0,0 +1,24 @@
+package Tue22SEP;
+
+public class ForEach1
+{
+    static void show(int[] arr)
+    {
+        for(int arr1 : arr)
+        {
+            System.out.print(arr1 + ",");
+        }
+
+    } 
+
+
+
+    public static void main(String[] args) 
+    {
+        show(new int[] {1,2,3,4,5});
+
+
+        
+    }
+    
+}

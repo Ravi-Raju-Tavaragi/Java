6653815179d4df2149d3ef0de11815c97d94a067
ravi <ravirajutavaragi@gmail.com>
2026-09-22 12:13:54 +0530

    Java Function programs

---
+++ Tue22SEP/SearchElement.java
@@ -0,0 +1,27 @@
+package Tue22SEP;
+
+public class SearchElement
+{
+    static boolean search(int[] arr, int key)
+    {
+        for(int X : arr)
+        {
+            if(X == key)
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+
+
+    public static void main(String[] args) 
+    {
+        int[] arr = {10, 20, 30, 50};
+        System.out.println(search(arr, 30));
+        System.out.println(search(arr, 60));     
+    }
+    
+}

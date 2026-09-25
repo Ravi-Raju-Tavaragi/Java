37e3180a036496c7639f44aec32d06c456c03920
ravi <ravirajutavaragi@gmail.com>
2026-09-25 11:25:34 +0530

    Java Function programs

---
+++ Fri25SEP/ArrayDSA.java
@@ -0,0 +1,12 @@
+package Fri25SEP;
+
+public class ArrayDSA 
+{
+    public static void main(String[] args)
+    {
+        int[] a = {10, 20, 30, 40};
+        System.out.println(a[0]);
+
+    }
+    
+}
